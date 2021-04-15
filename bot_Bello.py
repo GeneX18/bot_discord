500097b90ceb9b72cd1b16e548264fdeb7262ae6
GeneX18 <edo.gene@libero.it>
2021-04-15 16:10:57 +0200

    first command

--- bot_Bello.py
+++ bot_Bello.py
@@ -18,14 +18,6 @@ bot = commands.Bot(command_prefix='-', intents=intents)
 async def cerca(ctx):
     r = random.randint(1,7)
     await ctx.send("hai trovato {} euro!".format(r))
-
-@bot.event
-async def on_ready():
-    for guild in bot.guilds:
-        if guild.name == GUILD
-            break
-    members = '\n -'.join([member.name for member in guild.members])
-    print(members)
     
     
 bot.run(TOKEN)
